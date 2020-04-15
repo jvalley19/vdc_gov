171f94c326385389aeeb4fca1a6f2e0476676bf3
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2020-04-15 17:44:40 -0400

    q3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "HI dummy"
 
 chmod +x  ./Orchestration/OrchestrationService/ModuleConfigurationDeployment.ps1
-
+chmod +x ./Environments/SharedServices
 ./Orchestration/OrchestrationService/ModuleConfigurationDeployment.ps1 -DefinitionPath ./Environments/SharedServices/definition.json
