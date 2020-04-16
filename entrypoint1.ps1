23a82896e4fa75148d6d22c8cd8eedc81cd1447f
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2020-04-16 11:16:05 -0400

    87

--- entrypoint1.ps1
+++ entrypoint1.ps1
@@ -2,6 +2,7 @@
 $null = Find-Module -Name Az | Install-Module -Force
 $null = Install-Module Az.ResourceGraph -Force
 $null = Install-Module -Name Az.Accounts -Force
+$null = Install-Module -Name Pester -Force
 
 $secpasswd = ConvertTo-SecureString $env:SERVICE_PRINCIPAL_PASS -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($env:SERVICE_PRINCIPAL, $secpasswd)
@@ -13,4 +14,5 @@ Write-Host "HI dummy"
 Write-Debug Get-Location 
 #Write-Debug $(ls)
 ##chmod +x ./Environments/SharedServices
+$DebugPreference="Continue"
 ./Orchestration/OrchestrationService/ModuleConfigurationDeployment.ps1 -DefinitionPath ./Environments/SharedServices/definition.json
