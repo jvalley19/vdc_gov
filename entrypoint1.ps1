bab159c1b29c8cbad9517bf9d5b0236b6dc3a307
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2020-05-04 16:15:59 -0400

    a test

--- entrypoint1.ps1
+++ entrypoint1.ps1
@@ -34,4 +34,5 @@ Write-Host "Executing the cleanup script"
 
 ./Orchestration/OrchestrationService/Cleanup_Script.ps1
 
+
 Write-Host "The deployment was succesfull if: Exit code $LASTEXITCODE == 0" -Verbose
