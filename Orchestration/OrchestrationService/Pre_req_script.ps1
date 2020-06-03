b1f576ca2f350569e079ca215f6ebe8567e35f4c
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2020-06-03 15:29:55 -0400

    updated pre-req with some simple output

--- Orchestration/OrchestrationService/Pre_req_script.ps1
+++ Orchestration/OrchestrationService/Pre_req_script.ps1
@@ -1,4 +1,7 @@
-﻿##### Replace values with environment variables for the toolkit.subscription.json file
+﻿Write-Host `n"Running the Pre-Req script for the VDC toolkit" -ForegroundColor Cyan
+Write-Host `n"Setting Subscription ID and Tenant ID in the Config Files." -ForegroundColor Green
+
+##### Replace values with environment variables for the toolkit.subscription.json file
 $var = (Get-Content -Path .\Config\toolkit.subscription.json) | ConvertFrom-Json
 $var.Comments = "ToolKit for creating a new Virtual Data Center"
 $var.SubscriptionId = $ENV:SUBSCRIPTION_ID
@@ -22,12 +25,17 @@ $arti.Artifacts.SubscriptionId = $ENV:HUB_SUB_ID
 $arti.Artifacts.TenantId = $ENV:TENANT_ID
 $arti | ConvertTo-Json | Set-Content -Path .\Environments\_Common\subscriptions.json
 
+$arti = (Get-Content -Path .\Environments\_Common\subscriptions.json) | ConvertFrom-Json
+$arti.SPOKE.SubscriptionId = $ENV:SUBSCRIPTION_ID
+$arti.SPOKE.TenantId = $ENV:TENANT_ID
+$arti | ConvertTo-Json | Set-Content -Path .\Environments\_Common\subscriptions.json
+
 $onprem = (Get-Content -Path .\Environments\_Common\subscriptions.json) | ConvertFrom-Json
 $onprem.OnPremises.SubscriptionId = $ENV:SUBSCRIPTION_ID
 $onprem.OnPremises.TenantId = $ENV:TENANT_ID
 $onprem | ConvertTo-Json | Set-Content -Path .\Environments\_Common\subscriptions.json
 
-
+Write-Host `n"Checking password randomization"
 #### Check if random passwords are needed or if passwords are provided for the VM admin accounts and the Active Directory Account
 
 # Random Password Function
@@ -57,3 +65,4 @@ if (($null -eq $ENV:DOMAIN_ADMIN_USER_PWD) -or ("" -eq $ENV:DOMAIN_ADMIN_USER_PW
     $ENV:DOMAIN_ADMIN_USER_PWD = Get-RandomPassword
 }
 
+Write-Host `n"Passwords set for random"
